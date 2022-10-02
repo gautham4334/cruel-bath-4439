21bd5966dbf78390d8cb5c0266158f20ca2e6d38
Gautham Nandakumar <gautham4334@gmail.com>
2022-10-02 18:25:24 +0530

    pushed day-4

---
+++ index.js
@@ -0,0 +1,119 @@
+let livescores=document.querySelector("#navbar>p");
+livescores.addEventListener("mouseover",function(){
+    livescores.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+    let a=document.querySelector(".modals>div");
+    a.style.display="block";
+    let b=document.querySelector(".modals");
+    b.style.display="block";
+})
+livescores.addEventListener("mouseout",function(){
+    livescores.style.boxShadow="none";
+    let a=document.querySelector(".modals>div");
+    a.style.display="none";
+    let b=document.querySelector(".modals");
+    b.style.display="none";
+
+})
+let series=document.querySelector("#navbar>p+p");
+series.addEventListener("mouseover",function(){
+    series.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+series.addEventListener("mouseout",function(){
+    series.style.boxShadow="none";
+})
+let teams=document.querySelector("#navbar>p+p+p");
+teams.addEventListener("mouseover",function(){
+    teams.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+teams.addEventListener("mouseout",function(){
+    teams.style.boxShadow="none";
+})
+let news=document.querySelector("#navbar>p+p+p+p");
+news.addEventListener("mouseover",function(){
+    news.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+news.addEventListener("mouseout",function(){
+    news.style.boxShadow="none";
+})
+let videos=document.querySelector("#navbar>p+p+p+p+p+p");
+videos.addEventListener("mouseover",function(){
+    videos.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+videos.addEventListener("mouseout",function(){
+    videos.style.boxShadow="none";
+})
+let features=document.querySelector("#navbar>p+p+p+p+p");
+features.addEventListener("mouseover",function(){
+    features.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+features.addEventListener("mouseout",function(){
+    features.style.boxShadow="none";
+})
+let stats=document.querySelector("#navbar>p+p+p+p+p+p+p");
+stats.addEventListener("mouseover",function(){
+    stats.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+stats.addEventListener("mouseout",function(){
+    stats.style.boxShadow="none";
+})
+let fantay=document.querySelector(".header21");
+fantay.addEventListener("mouseover",function(){
+    fantay.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+fantay.addEventListener("mouseout",function(){
+    fantay.style.boxShadow="none";
+})
+let edition=document.querySelector(".header22>p");
+edition.addEventListener("mouseover",function(){
+    edition.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+edition.addEventListener("mouseout",function(){
+    edition.style.boxShadow="none";
+})
+let icon1=document.querySelector(".header22>img");
+icon1.addEventListener("mouseover",function(){
+    icon1.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+icon1.addEventListener("mouseout",function(){
+    icon1.style.boxShadow="none";
+})
+let icon2=document.querySelector(".header22>img+img");
+icon2.addEventListener("mouseover",function(){
+    icon2.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+icon2.addEventListener("mouseout",function(){
+    icon2.style.boxShadow="none";
+})
+let icon3=document.querySelector(".header22>img+img+img");
+icon3.addEventListener("mouseover",function(){
+    icon3.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+})
+icon3.addEventListener("mouseout",function(){
+    icon3.style.boxShadow="none";
+})
+let icon4=document.querySelector(".header22>img+img+img+img");
+icon4.addEventListener("mouseover",function(){
+    icon4.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+    
+})
+icon4.addEventListener("mouseout",function(){
+    icon4.style.boxShadow="none";
+
+})
+let icon5=document.querySelector(".header22>img+img+img+img+img");
+icon5.addEventListener("mouseover",function(){
+    icon5.style.boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+    
+})
+icon5.addEventListener("mouseout",function(){
+    icon5.style.boxShadow="none";
+    
+})
+// let search=document.querySelector(".searchbar");
+// let headers=document.querySelector(".header22");
+// let headers2=document.querySelector(".header21")
+// icon5.addEventListener("click",function(){
+//     search.style.display="inline";
+//     headers.style.display="none";
+//     headers2.style.display="none";
+// })
